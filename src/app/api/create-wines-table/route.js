ea6f914512a981274c545f1ae3901c2f0bb3a6f3
songezo_mtengwana <songezomtengwana@gmail.com>
2024-01-13 10:29:21 +0200

    api configuration

---
+++ src/app/api/create-wines-table/route.js
@@ -0,0 +1,21 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+ 
+export async function GET(request) {
+  try {
+    const result =
+      await sql`CREATE TABLE wines (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        year INTEGER NOT NULL,
+        type VARCHAR(20) NOT NULL CHECK (type IN ('Red', 'White', 'Rosé', 'White Blend', 'Red Blend')),
+        varietal VARCHAR(50) NOT NULL,
+        rating FLOAT CHECK (rating BETWEEN 1 AND 5),
+        consumed BOOLEAN,
+        date_consumed DATE
+      );`;
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
