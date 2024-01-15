3f6d5e5b06253a665dd991fffb445c0ede5db4d4
songezo_mtengwana <songezomtengwana@gmail.com>
2024-01-15 16:12:33 +0200

    add wine route update

--- src/app/api/add-wine/route.ts
+++ src/app/api/add-wine/route.ts
@@ -1,39 +1,44 @@
+import { WineData } from "@/app/model/wine.model";
 import { sql } from "@vercel/postgres";
+import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const name = searchParams.get("name");
-  const year = searchParams.get("year");
-  const type = searchParams.get("type");
-  const varietal = searchParams.get("varietal");
-  const rating = searchParams.get("rating");
-  const consumed = searchParams.get("consumed");
-  const date_consumed = searchParams.get("date_consumed");
+// export async function GET(request: Request) {
+//   const { searchParams } = new URL(request.url);
+//   const name = searchParams.get("name");
+//   const year = searchParams.get("year");
+//   const type = searchParams.get("type");
+//   const varietal = searchParams.get("varietal");
+//   const rating = searchParams.get("rating");
+//   const consumed = searchParams.get("consumed");
+//   const date_consumed = searchParams.get("date_consumed");
 
-  try {
-    await sql`INSERT INTO wines (name, year, type, varietal, rating, consumed, date_consumed) VALUES (${name}, ${year}, ${type}, ${varietal}, ${rating}, ${consumed}, ${date_consumed})`;
-  } catch (error) {
-    return NextResponse.json({ error }, { status: 500 });
+//   try {
+//     await sql`INSERT INTO wines (name, year, type, varietal, rating, consumed, date_consumed) VALUES (${data.name}, ${data.year}, ${data.type}, ${data.varietal}, ${data.rating}, ${data.consumed}, ${data.date_consumed})`;
+//   } catch (error) {
+//     return NextResponse.json({ error }, { status: 500 });
+//   }
+
+//   const pets = await sql`SELECT * FROM Wines;`;
+//   return NextResponse.json({ pets }, { status: 200 });
+// }
+
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+  if (!req.body) {
+    return NextResponse.json(
+      { error: "Invalid request body" },
+      { status: 400 }
+    );
   }
 
-  const pets = await sql`SELECT * FROM Wines;`;
-  return NextResponse.json({ pets }, { status: 200 });
+  try {
+    const { data }: { data: WineData } = req.body;
+    console.log(req.body);
+
+    await sql`INSERT INTO wines (name, year, type, varietal, rating, consumed, date_consumed) VALUES (${data.name}, ${data.year}, ${data.type}, ${data.varietal}, ${data.rating}, ${data.consumed}, ${data.date_consumed})`;
+    return NextResponse.json({ data }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error }, { status: 500 });
+  }
 }
-
-// import { sql } from "@vercel/postgres";
-// import { NextApiResponse, NextApiRequest } from "next";
-
-// const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-//   try {
-//     await sql`INSERT INTO wines (name, year, type, varietal, rating, consumed, date_consumed)
-//                 VALUES (${req.body.name}, ${req.body.year}, ${req.body.type}, ${req.body.type}, ${req.body.rating}, ${req.body.consumed}, ${req.body.date_consumed})`;
-
-//     res.status(201).json({ message: "Wine added successfully!" });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: "Failed to add wine" });
-//   }
-// };
-
-// export default handler;
